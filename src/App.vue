77a2843871a04ab6fce2564f1ebdb2673d0d5283
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-04 23:08:59 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="app">
+		<Keyboard @key-pressed="keyPress" />
+	</div>
+</template>
+
+<script>
+import Keyboard from "./components/Keyboard.vue";
+// import TextArea from "./components/TextArea.vue";
+
+export default {
+	name: "App",
+	components: {
+		Keyboard,
+	},
+	methods: {
+		keyPress(key) {
+			console.log(key);
+		}
+	}
+};
+</script>
+
+<style>
+#app {
+	font-family: "Cutive Mono", Avenir, Helvetica, Arial, sans-serif;
+	color: white;
+	font-size: 25px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 98vh;
+	width: 99vw;
+}
+</style>
---
+++ src/components/Keyboard.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="keyboard-container">
+		<div class="keys-container">
+			<div class="key-cap" v-for="key in this.keys" :data-key="key">{{key}}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Keyboard",
+	emits: ["key-pressed"],
+	methods: {
+		registerKeyEventListener() {
+			document.addEventListener("keyup", (event) => {
+				$(`.key-cap[data-key="${event.key}"]`).removeClass("pressed");
+			});
+
+			document.addEventListener("keydown", (event) => {
+				this.$emit("key-pressed", event.key);
+				$(`.key-cap[data-key="${event.key}"]`).addClass("pressed");
+			});
+		},
+	},
+	mounted() {
+		this.registerKeyEventListener();
+	},
+	data() {
+		return {
+			keys: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+				   "a", "s", "d", "f", "g", "h", "j", "k", "l",
+				   "z", "x", "c", "v", "b", "n", "m", ",", "."]
+		};
+	},
+};
+</script>
+
+<style scoped>
+.keys-container {
+	display: flex;
+	flex-wrap: wrap;
+	max-width: 600px;
+	gap: 0.9rem;
+	justify-content: center;
+}
+
+.key-cap {
+	font-weight: bolder;
+	border: 2px solid var(--border-color);
+	color: var(--main-color);
+	border-radius: 0.4rem;
+	width: 40px;
+	height: 40px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	user-select: none;
+	transition: all 0.15s;
+}
+
+.key-cap.pressed {
+	background-color: rgba(var(--main-color-rgb) / 0.4);
+}
+</style>
