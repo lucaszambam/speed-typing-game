4c60138be086a300bd534df7078acb4a9703448c
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-05 00:44:54 -0300

    create textcontainer component

---
+++ src/components/TextContainer.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="text-container">
+		<span>{{ this.text }}</span>
+	</div>
+</template>
+
+<script>
+import texts from "/src/assets/texts/texts.json";
+
+export default {
+	name: "TextContainer",
+    data() {
+        return {
+            text: ''
+        }
+    },
+	methods: {
+		getText() {
+            try {
+                return texts[(Math.floor(Math.random() * texts.length))].text;
+            } catch(err) {
+                console.error('[texts.json not found...]');
+                console.error(err);
+            }
+        }
+	},
+    beforeMount() {
+        this.text = this.getText();
+    }
+};
+</script>
+
+<style scoped>
+.text-container {
+    display: flex;
+    max-height: 130px;
+    overflow: hidden;
+    font-size: 30px;
+    font-weight: 500;
+}
+</style>
