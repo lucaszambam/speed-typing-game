f5279cacb92f04edf109398f205e9bb7de6b5e3f
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-05 19:18:38 -0300

    adjust text structure

--- src/components/TextContainer.vue
+++ src/components/TextContainer.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="text-container">
-		<span>{{ this.text }}</span>
+        <span v-for="word in this.words" class="word">
+		    <span v-for="letter in word.letters" class="letter" :class="{ current: letter.current }">
+                {{ letter.value }}
+            </span>
+        </span>
 	</div>
 </template>
 
@@ -9,23 +13,46 @@ import texts from "/src/assets/texts/texts.json";
 
 export default {
 	name: "TextContainer",
+    props: ["typed-letter"],
+    watch: { 
+        typedLetter: (newVal, oldVal) => {
+            console.log('Prop changed: ', newVal, ' | was: ', oldVal)
+        }
+    },
     data() {
         return {
-            text: ''
+            current: '',
+            words: [],
+            letters: [],
+            letter: {
+                current: false,
+                typed: false,
+                value: ''
+            }
         }
     },
 	methods: {
-		getText() {
+		initText() {
             try {
-                return texts[(Math.floor(Math.random() * texts.length))].text;
+                const text = texts[Math.floor(Math.random() * texts.length)].text;
+                const words = text.match(/\S+|\s+/g);
+
+                this.words = words.map((word, indexWord) => {
+                    const lettersObject = word.split('').map((letter, indexLetter) => ({
+                        current: (indexWord === 0 && indexLetter === 0),
+                        typed: false,
+                        value: letter
+                    }));
+                    return {letters: lettersObject};
+                });
             } catch(err) {
-                console.error('[texts.json not found...]');
+                console.error('[could not read texts.json...]');
                 console.error(err);
             }
         }
 	},
     beforeMount() {
-        this.text = this.getText();
+        this.initText();
     }
 };
 </script>
@@ -33,9 +60,19 @@ export default {
 <style scoped>
 .text-container {
     display: flex;
+    flex-wrap: wrap;
     max-height: 130px;
     overflow: hidden;
     font-size: 30px;
     font-weight: 500;
+}
+.word {
+    display: flex;
+}
+.letter {
+    width: 16px;
+}
+.letter.current {
+    color: var(--main-color);
 }
 </style>
