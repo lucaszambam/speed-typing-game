7af909fb82bed008404c1edfedfe20cbf7f2d7af
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-05 00:46:19 -0300

    add textcontainer component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
 	<div id="app">
-		<Keyboard @key-pressed="keyPress" />
+		<div class="central-area">
+			<TextContainer />
+			<Keyboard @key-pressed="keyPress" />
+		</div>
 	</div>
 </template>
 
 <script>
+import TextContainer from "./components/TextContainer.vue"; 
 import Keyboard from "./components/Keyboard.vue";
-// import TextArea from "./components/TextArea.vue";
 
 export default {
 	name: "App",
 	components: {
 		Keyboard,
+		TextContainer
 	},
 	methods: {
 		keyPress(key) {
@@ -31,5 +35,13 @@ export default {
 	align-items: center;
 	height: 98vh;
 	width: 99vw;
+}
+.central-area {
+	max-width: 50%;
+	display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+	gap: 4rem;
 }
 </style>
