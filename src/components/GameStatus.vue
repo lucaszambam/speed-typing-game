a91e3d92d2bd3984ead1c1aa864994671d6c9c87
Lucas Zambam <lucaszambam7896@gmail.com>
2023-08-12 22:46:18 -0300

    create GameStatus component

---
+++ src/components/GameStatus.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="gamestatus-container">
+		<div v-if="this.started" class="info-container">
+            <span>
+                Hold <span class="enter">enter</span> to restart.
+            </span>
+        </div>
+        <div v-else class="info-container">
+            <span>
+                Press any key to start.
+            </span>
+        </div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "GameStatus",
+    props: ['started'],
+	emits: ["restart"]
+}
+</script>
+
+<style scoped>
+.info-container > span {
+    opacity: 0.5;
+}
+.info-container span.enter {
+    border: 1px solid white;
+    border-radius: 0.4rem;
+    padding: 1px 6px;
+}
+</style>
